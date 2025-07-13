30d7f335833aac542c2f3b3b7695d32e46dcbc04
Sahadipankar <sahadepankar@gmail.com>
2025-07-13 16:40:57 +0530

    App.jsx modified

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
 
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
