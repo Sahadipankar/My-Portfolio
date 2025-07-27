ccc7d242e4d1f40353dbadd2ae959118031dc46c
Sahadipankar <sahadepankar@gmail.com>
2025-07-27 13:55:03 +0530

    Refactor Dashboard component layout to enhance project, skill, and timeline sections

--- dashboard/src/pages/sub-components/Dashboard.jsx
+++ dashboard/src/pages/sub-components/Dashboard.jsx
@@ -155,215 +155,205 @@ const Dashboard = () => {
                 </CardFooter>
               </Card>
             </div>
-            <Tabs>
-              <TabsContent>
-                <Card>
-                  <CardHeader className="px-7">
-                    <CardTitle>Projects</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <Table>
-                      <TableHeader>
-                        <TableRow>
-                          <TableHead>Title</TableHead>
-                          <TableHead className="hidden md:table-cell">
-                            Stack
-                          </TableHead>
-                          <TableHead className="hidden md:table-cell">
-                            Deployed
-                          </TableHead>
-                          <TableHead className="md:table-cell">
-                            Update
-                          </TableHead>
-                          <TableHead className="text-right">Visit</TableHead>
-                        </TableRow>
-                      </TableHeader>
-                      <TableBody>
-                        {projects && projects.length > 0 ? (
-                          projects.map((element) => {
-                            return (
-                              <TableRow className="bg-accent" key={element._id}>
-                                <TableCell>
-                                  <div className="font-medium">
-                                    {element.title}
-                                  </div>
-                                </TableCell>
-                                <TableCell className="hidden md:table-cell font-semibold uppercase">
-                                  {element.stack}
-                                </TableCell>
-                                <TableCell className="hidden md:table-cell font-semibold uppercase">
-                                  <Badge
-                                    className="text-xs"
-                                    variant="secondary"
-                                  >
-                                    {element.deployed}
-                                  </Badge>
-                                </TableCell>
-                                <TableCell className="md:table-cell">
-                                  <Link to={`/update/project/${element._id}`}>
-                                    <Button>Update</Button>
-                                  </Link>
-                                </TableCell>
-                                <TableCell className="text-right">
-                                  <Link
-                                    to={element.projectLink}
-                                    target="_blank"
-                                  >
-                                    <Button>Visit</Button>
-                                  </Link>
-                                </TableCell>
-                              </TableRow>
-                            );
-                          })
-                        ) : (
-                          <TableRow>
-                            <TableCell className="text-3xl overflow-y-hidden">
-                              You have not added any project.
+            <Card>
+              <CardHeader className="px-7">
+                <CardTitle>Projects</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Title</TableHead>
+                      <TableHead className="hidden md:table-cell">
+                        Stack
+                      </TableHead>
+                      <TableHead className="hidden md:table-cell">
+                        Deployed
+                      </TableHead>
+                      <TableHead className="md:table-cell">
+                        Update
+                      </TableHead>
+                      <TableHead className="text-right">Visit</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {projects && projects.length > 0 ? (
+                      projects.map((element) => {
+                        return (
+                          <TableRow className="bg-accent" key={element._id}>
+                            <TableCell>
+                              <div className="font-medium">
+                                {element.title}
+                              </div>
+                            </TableCell>
+                            <TableCell className="hidden md:table-cell font-semibold uppercase">
+                              {element.stack}
+                            </TableCell>
+                            <TableCell className="hidden md:table-cell font-semibold uppercase">
+                              <Badge
+                                className="text-xs"
+                                variant="secondary"
+                              >
+                                {element.deployed}
+                              </Badge>
+                            </TableCell>
+                            <TableCell className="md:table-cell">
+                              <Link to={`/update/project/${element._id}`}>
+                                <Button>Update</Button>
+                              </Link>
+                            </TableCell>
+                            <TableCell className="text-right">
+                              <Link
+                                to={element.projectLink}
+                                target="_blank"
+                              >
+                                <Button>Visit</Button>
+                              </Link>
                             </TableCell>
                           </TableRow>
-                        )}
-                      </TableBody>
-                    </Table>
-                  </CardContent>
-                </Card>
-              </TabsContent>
-            </Tabs>
-            <Tabs>
-              <TabsContent>
-                <Card>
-                  <CardHeader className="px-7 gap-3">
-                    <CardTitle>Skills</CardTitle>
-                  </CardHeader>
-                  <CardContent className="grid sm:grid-cols-2 gap-4">
-                    {skills && skills.length > 0 ? (
-                      skills.map((element) => {
-                        return (
-                          <Card key={element._id}>
-                            <CardHeader>{element.title}</CardHeader>
-                            <CardFooter>
-                              <Progress value={element.proficiency} />
-                            </CardFooter>
-                          </Card>
                         );
                       })
                     ) : (
-                      <p className="text-3xl">You have not added any skill.</p>
+                      <TableRow>
+                        <TableCell className="text-3xl overflow-y-hidden">
+                          You have not added any project.
+                        </TableCell>
+                      </TableRow>
                     )}
-                  </CardContent>
-                </Card>
-              </TabsContent>
-            </Tabs>
-            <Tabs>
-              <TabsContent className="grid min-[1050px]:grid-cols-2 gap-4">
-                <Card>
-                  <CardHeader className="px-7">
-                    <CardTitle>Software Applications</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <Table>
-                      <TableHeader>
-                        <TableRow>
-                          <TableHead>Name</TableHead>
-                          <TableHead className="md:table-cell">Icon</TableHead>
-                          <TableHead className="md:table-cell text-center">
-                            Action
-                          </TableHead>
-                        </TableRow>
-                      </TableHeader>
-                      <TableBody>
-                        {softwareApplications &&
-                          softwareApplications.length > 0 ? (
-                          softwareApplications.map((element) => {
-                            return (
-                              <TableRow className="bg-accent" key={element._id}>
-                                <TableCell className="font-medium">
-                                  {element.name}
-                                </TableCell>
-                                <TableCell className="md:table-cell">
-                                  <img
-                                    className="w-7 h-7"
-                                    src={element.svg && element.svg.url}
-                                    alt={element.name}
+                  </TableBody>
+                </Table>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="px-7 gap-3">
+                <CardTitle>Skills</CardTitle>
+              </CardHeader>
+              <CardContent className="grid sm:grid-cols-2 gap-4">
+                {skills && skills.length > 0 ? (
+                  skills.map((element) => {
+                    return (
+                      <Card key={element._id}>
+                        <CardHeader>{element.title}</CardHeader>
+                        <CardFooter>
+                          <Progress value={element.proficiency} />
+                        </CardFooter>
+                      </Card>
+                    );
+                  })
+                ) : (
+                  <p className="text-3xl">You have not added any skill.</p>
+                )}
+              </CardContent>
+            </Card>
+            <div className="grid min-[1050px]:grid-cols-2 gap-4">
+              <Card>
+                <CardHeader className="px-7">
+                  <CardTitle>Software Applications</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Name</TableHead>
+                        <TableHead className="md:table-cell">Icon</TableHead>
+                        <TableHead className="md:table-cell text-center">
+                          Action
+                        </TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {softwareApplications &&
+                        softwareApplications.length > 0 ? (
+                        softwareApplications.map((element) => {
+                          return (
+                            <TableRow className="bg-accent" key={element._id}>
+                              <TableCell className="font-medium">
+                                {element.name}
+                              </TableCell>
+                              <TableCell className="md:table-cell">
+                                <img
+                                  className="w-7 h-7"
+                                  src={element.svg && element.svg.url}
+                                  alt={element.name}
+                                />
+                              </TableCell>
+                              <TableCell className="md:table-cell  text-center">
+                                {appLoading && appId === element._id ? (
+                                  <SpecialLoadingButton
+                                    content={"Deleting"}
+                                    width={"w-fit"}
                                   />
-                                </TableCell>
-                                <TableCell className="md:table-cell  text-center">
-                                  {appLoading && appId === element._id ? (
-                                    <SpecialLoadingButton
-                                      content={"Deleting"}
-                                      width={"w-fit"}
-                                    />
-                                  ) : (
-                                    <Button
-                                      onClick={() =>
-                                        handleDeleteSoftwareApp(element._id)
-                                      }
-                                    >
-                                      Delete
-                                    </Button>
-                                  )}
-                                </TableCell>
-                              </TableRow>
-                            );
-                          })
-                        ) : (
-                          <TableRow>
-                            <TableCell className="text-3xl overflow-y-hidden">
-                              You have not added any skill.
-                            </TableCell>
-                          </TableRow>
-                        )}
-                      </TableBody>
-                    </Table>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="px-7 flex items-center justify-between flex-row">
-                    <CardTitle>Timeline</CardTitle>
-                    <Button onClick={gotoMangeTimeline} className="w-fit">
-                      Manage Timeline
-                    </Button>
-                  </CardHeader>
-                  <CardContent>
-                    <Table>
-                      <TableHeader>
+                                ) : (
+                                  <Button
+                                    onClick={() =>
+                                      handleDeleteSoftwareApp(element._id)
+                                    }
+                                  >
+                                    Delete
+                                  </Button>
+                                )}
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })
+                      ) : (
                         <TableRow>
-                          <TableHead>Title</TableHead>
-                          <TableHead className="md:table-cell">From</TableHead>
-                          <TableHead className="md:table-cell text-right">
-                            To
-                          </TableHead>
+                          <TableCell className="text-3xl overflow-y-hidden">
+                            You have not added any skill.
+                          </TableCell>
                         </TableRow>
-                      </TableHeader>
-                      <TableBody>
-                        {timeline && timeline.length > 0 ? (
-                          timeline.map((element) => {
-                            return (
-                              <TableRow className="bg-accent" key={element._id}>
-                                <TableCell className="font-medium">
-                                  {element.title}
-                                </TableCell>
-                                <TableCell className="md:table-cell">
-                                  {element.timeline.from}
-                                </TableCell>
-                                <TableCell className="md:table-cell  text-right">
-                                  {element.timeline.to}
-                                </TableCell>
-                              </TableRow>
-                            );
-                          })
-                        ) : (
-                          <TableRow>
-                            <TableCell className="text-3xl overflow-y-hidden">
-                              You have not added any timeline.
-                            </TableCell>
-                          </TableRow>
-                        )}
-                      </TableBody>
-                    </Table>
-                  </CardContent>
-                </Card>
-              </TabsContent>
-            </Tabs>
+                      )}
+                    </TableBody>
+                  </Table>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="px-7 flex items-center justify-between flex-row">
+                  <CardTitle>Timeline</CardTitle>
+                  <Button onClick={gotoMangeTimeline} className="w-fit">
+                    Manage Timeline
+                  </Button>
+                </CardHeader>
+                <CardContent>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Title</TableHead>
+                        <TableHead className="md:table-cell">From</TableHead>
+                        <TableHead className="md:table-cell text-right">
+                          To
+                        </TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {timeline && timeline.length > 0 ? (
+                        timeline.map((element) => {
+                          return (
+                            <TableRow className="bg-accent" key={element._id}>
+                              <TableCell className="font-medium">
+                                {element.title}
+                              </TableCell>
+                              <TableCell className="md:table-cell">
+                                {element.timeline.from}
+                              </TableCell>
+                              <TableCell className="md:table-cell  text-right">
+                                {element.timeline.to}
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })
+                      ) : (
+                        <TableRow>
+                          <TableCell className="text-3xl overflow-y-hidden">
+                            You have not added any timeline.
+                          </TableCell>
+                        </TableRow>
+                      )}
+                    </TableBody>
+                  </Table>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </main>
       </div>
