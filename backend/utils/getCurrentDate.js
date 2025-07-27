aa9ded1e7cf120fc6dee4426cd9d5a232d60b729
Sahadipankar <sahadepankar@gmail.com>
2025-07-27 19:24:43 +0530

    Refactor getCurrentDate function to utilize date-fns-tz for improved date formatting

--- backend/utils/getCurrentDate.js
+++ backend/utils/getCurrentDate.js
@@ -1,14 +1,7 @@
+import { formatInTimeZone } from 'date-fns-tz';
+
 export function getCurrentDate() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate()).padStart(2, '0');
-    let hours = now.getHours();
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    const hoursStr = String(hours).padStart(2, '0');
-    return `${day}-${month}-${year}_${hoursStr}-${minutes}-${seconds}_${ampm}`;
+    const timeZone = 'Asia/Kolkata';
+    // Format: DD-MM-YYYY_hh-mm-ss_AM/PM
+    return formatInTimeZone(new Date(), timeZone, "dd-MM-yyyy_hh-mm-ss_a");
 }
